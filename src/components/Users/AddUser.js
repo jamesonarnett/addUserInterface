da18d08fb10d1074d5ff18b95e0d642744e39153
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2021-12-07 20:54:13 -0600

    App user interface/Style created
    Adding validation/resetting logic next//////

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card.js";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredAge, enteredUsername);
+  };
+
+  const userNameChageHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" onChange={userNameChageHandler} />
+
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
